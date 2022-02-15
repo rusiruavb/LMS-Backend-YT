5b072f9e91454ba98b1dba8b19688c7b2a2f0e94
rusiruavb <rusiruavb98@gmail.com>
2022-02-16 01:15:28 +0530

    Implment server configurations

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("Hello YouTube");
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res, next) => {
+  res.send("<h2>📚 Library Management System API</h2>");
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is up and running on PORT ${PORT}`);
+});
